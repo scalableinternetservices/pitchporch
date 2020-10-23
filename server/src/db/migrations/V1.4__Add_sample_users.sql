1491dc9962ba3cdff328c2a1fd17df36339b66f7
Stewart Dulaney <sdulaney@users.noreply.github.com>
2020-10-23 07:38:13 -0700

    Add seed data for users and projects

---
+++ server/src/db/migrations/V1.4__Add_sample_users.sql
@@ -0,0 +1,5 @@
+insert into `user` (`email`, `userType`, `name`) values ('bill@microsoft.com', 'USER', 'Bill Gates');
+insert into `user` (`email`, `userType`, `name`) values ('mark@facebook.com', 'USER', 'Mark Zuckerberg');
+insert into `user` (`email`, `userType`, `name`) values ('jeff@amazon.com', 'USER', 'Jeff Bezos');
+insert into `user` (`email`, `userType`, `name`) values ('larry@google.com', 'USER', 'Larry Page');
+insert into `user` (`email`, `userType`, `name`) values ('steve@apple.com', 'USER', 'Steve Jobs');
