9839bc8f965c3c5c856d7ad3e873f7daa2c88da9
denise-wang <denisew2212@gmail.com>
2020-11-12 21:55:19 -0800

    added queries to fetchProject.tsx

---
+++ web/src/view/page/fetchProject.tsx
@@ -0,0 +1,27 @@
+import { gql } from '@apollo/client'
+
+export const fragmentProject = gql`
+  fragment Project on Project {
+    id
+    name
+    email
+  }
+`
+
+export const fetchProjects = gql`
+  query FetchProjects {
+    projects {
+      ...Project
+    }
+  }
+  ${fragmentProject}
+`
+
+export const fetchProject = gql`
+  query FetchProject($projectId: Int!) {
+    project(projectId: $projectId) {
+      ...Project
+    }
+  }
+  ${fragmentProject}
+`
